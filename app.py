7515643d465e770438f3b98226de1d1ad77114b6
Gaberoids <figueiropas@hotmail.com>
2021-01-31 07:07:07 +0000

    initial commit. procfile and requerements and env.py created

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
 import os
+# library containing basic funct of python
+from flask import (Flask, flash, render_template, redirect, request, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, chech_password_hash
+
+
+app = Flask(__name__)
+
