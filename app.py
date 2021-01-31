cac264287fbfd4f87ac9a6b177fdefeeb7defc0e
Gaberoids <figueiropas@hotmail.com>
2021-01-31 07:20:55 +0000

    connecting app to heroku

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 # library containing basic funct of python
-from flask import (Flask, flash, render_template, redirect, request, url_for)
+from flask import (Flask,
+                    flash, render_template, redirect, request, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, chech_password_hash
@@ -8,3 +9,8 @@ from werkzeug.security import generate_password_hash, chech_password_hash
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
